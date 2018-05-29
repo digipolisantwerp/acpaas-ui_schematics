22e76f32f6409a1f6e549be4ae6a63b157eb3dba
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-05-29 14:27:15 +0200

    add fallback
    * add fallback setup files
    * generate fallback files
    * update workspace config
    * ignore fallback files in tsc

--- src/package/index.ts
+++ src/package/index.ts
@@ -3,7 +3,7 @@ import {
 	SchematicContext,
 	Tree,
 	chain,
-	externalSchematic,
+	// externalSchematic, TODO: restore once https://github.com/angular/devkit/issues/475 is resolved
 	template,
 	apply,
 	url,
@@ -14,8 +14,9 @@ import {
 	branchAndMerge,
 } from '@angular-devkit/schematics';
 import { strings } from '@angular-devkit/core';
+import { WorkspaceProject, WorkspaceSchema } from '@angular-devkit/core/src/workspace';
 
-import { getWorkspace } from '../utils/workspace';
+import { getWorkspace, getWorkspacePath } from '../utils/workspace';
 import { parseName } from '../utils/name';
 import { buildSelector } from '../utils/selector';
 import { validateName, validateHtmlSelector } from '../utils/validation';
@@ -26,13 +27,8 @@ const cleanupGeneratedLib = (options: any): Rule => {
 		const workspace = getWorkspace(tree);
 		const project = getProjectFromOptions(workspace, options);
 
-		tree.getDir(project.packageRoot)
-          .visit(filePath => {
-			console.log(filePath);
-
-		  });
-
-		// tree.delete(`${project.packageRoot}/src/public_api.ts`);
+		tree.delete(`${project.packageRoot}/src/public_api.ts`);
+		// TODO: remove generated files
 
 		return tree;
 	};
@@ -43,18 +39,21 @@ const generatePackage = (options: any): Rule => {
 		const workspace = getWorkspace(tree);
 		const project = getProjectFromOptions(workspace, options);
 
-		options.name = project.name;
-		options.path = project.path;
-		options.packageRoot = project.packageRoot;
-		options.selector = buildSelector(options, project.prefix);
+		const templateOptions: any = {
+			...options,
+			name: project.name,
+			path: project.path,
+			packageRoot: project.packageRoot,
+			selector: buildSelector(options, project.prefix),
+		};
 
-		validateName(options.name);
-		validateHtmlSelector(options.selector);
+		validateName(templateOptions.name);
+		validateHtmlSelector(templateOptions.selector);
 
 		const templateSource = apply(url('./files'), [
 			template({
 				...strings,
-				...options,
+				...templateOptions,
 			}),
 			move(project.path),
 		]);
@@ -68,10 +67,109 @@ const generatePackage = (options: any): Rule => {
 	};
 };
 
+const generateLibrary = (options: any): Rule => {
+	return (tree: Tree, _context: SchematicContext) => {
+		const workspace = getWorkspace(tree);
+		const project = getProjectFromOptions(workspace, options);
+
+		const templateOptions: any = {
+			...options,
+			name: project.name,
+			path: project.path,
+			packageRoot: project.packageRoot,
+			selector: buildSelector(options, project.prefix),
+		};
+
+		validateName(templateOptions.name);
+		validateHtmlSelector(templateOptions.selector);
+
+		templateOptions.packageName = project.name;
+		templateOptions.distRoot = `dist/${project.name}`;
+		templateOptions.relativePathToWorkspaceRoot = '../..';
+		templateOptions.entryFile = 'index';
+		templateOptions.prefix = project.prefix;
+
+		const templateSource = apply(url('./fallback'), [
+			template({
+				...strings,
+				...templateOptions,
+			}),
+			move(project.path),
+		]);
+
+		return chain([
+			branchAndMerge(
+				mergeWith(templateSource, MergeStrategy.Overwrite),
+				MergeStrategy.AllowOverwriteConflict
+			)
+		])(tree, _context);
+	};
+};
+
+const addProjectToWorkspace = (options: any): Rule => {
+	return (tree: Tree, _context: SchematicContext) => {
+		const workspace = getWorkspace(tree);
+		const project = getProjectFromOptions(workspace, options);
+
+		if (workspace.projects[project.name]) {
+			throw new Error(`Project '${name}' already exists in workspace.`);
+		}
+
+		// Add project to workspace.
+		workspace.projects[project.name] = {
+			root: project.packageRoot,
+			sourceRoot: `${project.packageRoot}/src`,
+			projectType: "library",
+			prefix: project.prefix,
+			architect: {
+				build: {
+					builder: "@angular-devkit/build-ng-packagr:build",
+					options: {
+						tsConfig: `${project.packageRoot}/tsconfig.lib.json`,
+						project: `${project.packageRoot}/ng-package.json`,
+					},
+					configurations: {
+						production: {
+							project: `${project.packageRoot}/ng-package.prod.json`,
+						},
+					},
+				},
+				test: {
+					builder: "@angular-devkit/build-angular:karma",
+					options: {
+						main: `${project.packageRoot}/src/test.ts`,
+						tsConfig: `${project.packageRoot}/tsconfig.spec.json`,
+						karmaConfig: `${project.packageRoot}/karma.conf.js`,
+					},
+				},
+				lint: {
+					builder: "@angular-devkit/build-angular:tslint",
+					options: {
+						tsConfig: [
+							`${project.packageRoot}/tsconfig.lib.json`,
+							`${project.packageRoot}/tsconfig.spec.json`,
+						],
+						exclude: [
+							"**/node_modules/**",
+						],
+					},
+				},
+			},
+		};
+
+		tree.overwrite(getWorkspacePath(tree), JSON.stringify(workspace, null, 2));
+
+		return tree;
+	};
+};
+
 export default function acpaasUIPackage(options: any): Rule {
 	return chain([
-		externalSchematic('@schematics/angular', 'library', options),
-		cleanupGeneratedLib(options),
+		// externalSchematic('@schematics/angular', 'library', options), TODO: restore once https://github.com/angular/devkit/issues/475 is resolved
+		// cleanupGeneratedLib(options), TODO: restore once https://github.com/angular/devkit/issues/475 is resolved
+		addProjectToWorkspace(options), // TODO: remove once https://github.com/angular/devkit/issues/475 is resolved
+		generateLibrary(options), // TODO: remove once https://github.com/angular/devkit/issues/475 is resolved
 		generatePackage(options),
 	]);
 }
+
